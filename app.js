28affbabc5449b1befd7369599101edfe08e55de
Balogun Olamide <olamidebalo214@gmail.com>
2021-10-27 21:13:25 +0100

    first commit

--- app.js
+++ app.js
@@ -17,11 +17,14 @@ app.set("view engine", "ejs");
 const dbURI = "mongodb+srv://olamide:1234@cluster0.f7hgm.mongodb.net/node-auth";
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => app.listen(5000))
+  .then((result) => app.listen(port))
   .catch((err) => console.log(err));
+
+// port
+const port = process.env.PORT || 5000;
 
 // routes
 app.get("*", checkUser);
-app.get("/", (req, res) => res.render("home"));
-app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
+app.get("/", requireAuth, (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
